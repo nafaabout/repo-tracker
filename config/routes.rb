e02bf123a234f8f84ceff3da9343786dd079375e
Nafaa Boutefer <nafaabout@gmail.com>
2021-07-26 18:50:58 +0100

    Refactor: convert repositories routes to resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "repositories", to: "repositories#index", as: :repositories
-  get "repository/:id", to: "repositories#show", as: :repository
-  get "repositories/new", to: "repositories#new", as: :new_repository
-  post "repositories", to: "repositories#create", as: :create_repository
+  resources :repositories
 end
--- spec/system/manage_repositories_spec.rb
+++ spec/system/manage_repositories_spec.rb
@@ -5,6 +5,15 @@ RSpec.describe "ManageRepositories", type: :system do
     driven_by(:rack_test)
   end
 
+  it "Displays all repositories in index" do
+    repositories = Fabricate.times(3, :repository)
+    visit repositories_path
+
+    repositories.each do |repo|
+      expect(page).to have_link(repo.full_name)
+    end
+  end
+
   it "Adds a new repository" do
     visit new_repository_path
 
@@ -15,12 +24,4 @@ RSpec.describe "ManageRepositories", type: :system do
     expect(page).to have_link("Microsoft/vscode")
   end
 
-  it "Displays all repositories in index" do
-    repositories = Fabricate.times(3, :repository)
-    visit repositories_path
-
-    repositories.each do |repo|
-      expect(page).to have_link(repo.full_name)
-    end
-  end
 end
