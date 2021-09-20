b0e71400ad1abdc2fc735fa9e41167656fb8522a
Nafaa Boutefer <nafaabout@gmail.com>
2021-09-20 15:21:22 +0100

    Remove annotations from models specs

--- spec/models/owner_spec.rb
+++ spec/models/owner_spec.rb
@@ -1,17 +1,3 @@
-# frozen_string_literal: true
-
-# == Schema Information
-#
-# Table name: owners
-#
-#  id         :binary           not null, primary key
-#  avatar_url :string
-#  login      :string           not null
-#  name       :string           not null
-#  owner_type :string           not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
 require 'rails_helper'
 
 RSpec.describe Owner, type: :model do
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,12 +1,3 @@
-# == Schema Information
-#
-# Table name: topics
-#
-#  id         :uuid             not null, primary key
-#  name       :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
 require 'rails_helper'
 
 RSpec.describe Topic, type: :model do
