bc05c1d3c155261ed87d7537b4ad9a519f9f56d6
Nafaa Boutefer <nafaabout@gmail.com>
2021-10-19 15:01:04 +0100

    Spec pull return values

--- spec/services/tags_pullers/dev_to_tags_puller_spec.rb
+++ spec/services/tags_pullers/dev_to_tags_puller_spec.rb
@@ -6,17 +6,22 @@ RSpec.describe TagsPullers::DevToTagsPuller, type: :service do
     let(:tags) { generate_tags_response_body(3) }
     let(:dev_to_tags_api_url) { 'https://dev.to/api/tags' }
 
-    it 'calls dev api tags endpoint' do
+    before do
       stub_request(:get, dev_to_tags_api_url)
         .with(query: query)
         .to_return(status: 200, body: tags.to_json)
+    end
 
+    it 'calls dev api tags endpoint' do
       TagsPullers::DevToTagsPuller.pull
 
       expect(a_request(:get, dev_to_tags_api_url).with(query: query)).to have_been_made.once
     end
 
-    it 'returns an array of tags'
+    it 'returns an array of tags' do
+      expect(TagsPullers::DevToTagsPuller.pull).to eq(tags)
+    end
+
     context 'when no params specified' do
       it 'defaults to page: 1 and per_page: 10'
     end
