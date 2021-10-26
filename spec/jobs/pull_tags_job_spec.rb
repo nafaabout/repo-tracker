ccef664cde3a3f4ce767118169b970d080a8ae55
Nafaa Boutefer <nafaabout@gmail.com>
2021-10-26 17:26:43 +0100

    Clean specs

--- spec/jobs/pull_tags_job_spec.rb
+++ spec/jobs/pull_tags_job_spec.rb
@@ -1,9 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe PullTagsJob, type: :job do
-  let(:page) { 1 }
-  let(:per_page) { 10 }
-  let(:tags) { generate_tags_response_body(per_page) }
   let(:dev_to_tags_api_url) { 'https://dev.to/api/tags' }
   let!(:platform) { Fabricate(:platform_with_no_tag, name: 'Dev.to') }
   let(:tags_puller) { Tags::Puller.new(platform.name) }
@@ -13,12 +10,17 @@ RSpec.describe PullTagsJob, type: :job do
   end
 
   context 'when no arguments provided for page and per_page' do
+    let(:tags) { generate_tags_response_body(10) }
+
+    it 'pulls 10 tags from page 1' do
+      expect(tags_puller).to receive(:pull)
+        .with(page: 1, per_page: 10)
+
+      PullTagsJob.perform_now(platform: platform)
+    end
+
     it 'pulls tags from the given platform' do
       stub_tags_request(tags: tags)
-
-      expect(tags_puller).to receive(:pull)
-        .with(page: page, per_page: per_page)
-        .and_yield(tags)
 
       expect do
         PullTagsJob.perform_now(platform: platform)
@@ -27,19 +29,14 @@ RSpec.describe PullTagsJob, type: :job do
   end
 
   context 'if page and per_page given' do
-    let(:page) { rand(2..10) }
-    let(:per_page) { rand(5..20) }
+    let(:page) { rand(10) }
+    let(:per_page) { rand(20) }
 
     it 'pulls that specific tags page from the given platform' do
-      stub_tags_request(tags: tags, query: { page: page, per_page: per_page })
-
       expect(tags_puller).to receive(:pull)
         .with(page: page, per_page: per_page)
-        .and_yield(tags)
 
-      expect do
-        PullTagsJob.perform_now(platform: platform, page: page, per_page: per_page)
-      end.to change { Tag.count }.to(per_page)
+      PullTagsJob.perform_now(platform: platform, page: page, per_page: per_page)
     end
   end
 end
